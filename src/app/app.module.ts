6c021581b0e33ee96176b84c79d2fbbda368e9e9
RuslanHaziev <hazievaleteya@yandex.ru>
2024-01-12 11:20:32 +0500

    feat: homewor-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HeaderModule} from './components/header/header.module';
+import {ProductsListModule} from './pages/products-list/products-list.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-    providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HeaderModule,
+        ProductsListModule,
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
